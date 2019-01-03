3c5e1d401a7293b80ec8736c23a91e66dcc46740
Adam Brown <adam.brown@factorybucket.com>
2019-01-03 09:58:52 -0500

    Added support for polymorphic relationships (needs refactor)

--- src/Jedrzej/Sortable/Criterion.php
+++ src/Jedrzej/Sortable/Criterion.php
@@ -55,20 +55,44 @@ class Criterion
     {
         $sortMethod = 'sort' . studly_case($this->getField());
 
+        /**
+         * @todo: allow passing associative arrays (or fluent QB calls), instead of if/else calls below.
+         * e.g. $relations['primary_table'], $relations['poly_key'], etc.
+         * BUT if need to provide a full associative array for each, is that better than QB?
+         * Sortable too limited without fluent calls (or even with) for normal/flipped/pivot/poly/etc?
+         */
+
         if (method_exists($builder->getModel(), $sortMethod)) {
             call_user_func([$builder->getModel(), $sortMethod], $builder, $this->getOrder());
         } else if (false !== strpos($this->getField(), '.')) {
             $relation_keys = explode('.', $this->getField());
-            if (isset($relation_keys[4]) && $relation_keys[4] === 'flip') {
-                // main table[0] . join_table[1] . foreign_key[2] . sort_column[3] . (literal word) flip[4]
-                if (! collect($builder->getQuery()->joins)->pluck('table')->contains($relation_keys[1])) {
-                    // use foreign key in primary table
-                    $builder->leftJoin($relation_keys[1], $relation_keys[0] . '.' . $relation_keys[2], '=', $relation_keys[1] . '.id');
+
+            if (in_array('flip', $relation_keys, true)) {
+                // .flip means the FK lives on the main table, not the join table.
+                if (isset($relation_keys[4]) && $relation_keys[4] === 'flip') {
+
+                    // Flipped Join, FK on local table:  main_table[0].join_table[1].foreign_key[2].sort_by_field[3].flip[4]
+                    if (! collect($builder->getQuery()->joins)->pluck('table')->contains($relation_keys[1])) {
+                        $builder->leftJoin($relation_keys[1], $relation_keys[0] . '.' . $relation_keys[2], '=', $relation_keys[1] . '.id');
+                    }
+                    $builder->orderBy($relation_keys[1] . '.' . $relation_keys[3], $this->getOrder())
+                        ->select($relation_keys[0] . '.*');       // just to avoid fetching anything from joined table
+                } elseif (isset($relation_keys[6]) && $relation_keys[6] === 'flip') {
+
+                    // Polymorphic Relationships:  main_table[0].poly_table[1].poly_foreign_key[2].join_table[3].foreign_key[4].sort_by_field[5].flip[6]
+                    if (! collect($builder->getQuery()->joins)->pluck('table')->contains($relation_keys[1])) {
+                        $builder->leftJoin($relation_keys[1], $relation_keys[0] . '.' . $relation_keys[2], '=', $relation_keys[1] . '.id');
+                    }
+                    if (! collect($builder->getQuery()->joins)->pluck('table')->contains($relation_keys[3])) {
+                        // use foreign key in primary table
+                        $builder->leftJoin($relation_keys[3], $relation_keys[1] . '.' . $relation_keys[4], '=', $relation_keys[3] . '.id');
+                    }
+                    $builder->orderBy($relation_keys[3] . '.' . $relation_keys[5], $this->getOrder())
+                        ->select($relation_keys[0] . '.*');       // just to avoid fetching anything from joined table
                 }
-                $builder->orderBy($relation_keys[1] . '.' . $relation_keys[3], $this->getOrder())
-                    ->select($relation_keys[0] . '.*');       // just to avoid fetching anything from joined table
-            } elseif (isset($relation_keys[4]) && $relation_keys[4]==='pivot') {
-                // main_table [0] . join_table [1] . foreign_key [2] . sort_column [3] . (literal word) pivot [4] . pivot_table [5] . local_key [6]
+            } elseif (isset($relation_keys[4]) && $relation_keys[4] === 'pivot') {
+
+                // M2M Sort, with Pivot:  main_table[0].join_table[1].foreign_key[2].sort_by_field[3].pivot[4].pivot_table[5].local_key[6]
                 $pivotTable = $relation_keys[5];
                 $localKey = $relation_keys[6];
 
@@ -79,7 +103,8 @@ class Criterion
                 $builder->orderBy($relation_keys[1] . '.' . $relation_keys[3], $this->getOrder())
                     ->select($relation_keys[0] . '.*');
             } else {
-                // use foreign key in joined table
+
+                // Normal Join, FK on related table:  main_table[0].join_table[1].foreign_key[2].sort_by_field[3]
                 if (! collect($builder->getQuery()->joins)->pluck('table')->contains($relation_keys[1])) {
                     $builder->leftJoin($relation_keys[1], $relation_keys[1] . '.' . $relation_keys[2], '=', $relation_keys[0] . '.id');
                 }
